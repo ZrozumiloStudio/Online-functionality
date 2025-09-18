d356570852c311fe7b44a9bd99ec9bd59880e32c
Zroz Dev <150717367+ZrozumiloStudio@users.noreply.github.com>
2025-09-18 19:07:22 +0300

    Update mod1.js

--- CF 10K1Y Edition/Addons/mod1.js
+++ CF 10K1Y Edition/Addons/mod1.js
@@ -1,11 +1,10 @@
-function registerMod() {
+(function(){
     const btn = document.createElement('button');
     btn.textContent = 'test';
-
     Object.assign(btn.style, {
         position: 'fixed',
         top: '20px',
-        right: '20px',
+        right: '150px',
         padding: '10px 20px',
         background: '#333',
         color: '#fff',
@@ -14,13 +13,7 @@ function registerMod() {
         cursor: 'pointer',
         zIndex: 9999
     });
-
-    btn.onclick = () => {
-        alert('Test button clicked!');
-    };
-
+    btn.onclick = () => alert('Test button clicked!');
     document.body.appendChild(btn);
-
-    console.log('[TestButton Mod] test');
-}
-
+    console.log('[TestButton Mod] кнопка test создана');
+})();
